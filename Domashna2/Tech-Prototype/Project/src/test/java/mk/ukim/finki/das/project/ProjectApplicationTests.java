ddf9308f2c190d081568d756efbedc25e9289016
Никола Салтиров <nikola.saltirov04@gmail.com>
2024-11-25 11:40:26 +0100

    setup d2

---
+++ Domashna2/Tech-Prototype/Project/src/test/java/mk/ukim/finki/das/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.das.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
